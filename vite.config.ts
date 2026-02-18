07e3285992616c91d5a4e09e3ecddfaa4b70869e
Barrrox <matheusbarros173@gmail.com>
2026-02-17 22:14:41 -0300

    FIX: nome diferente do repositorio

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // O 'base' deve ser o nome do seu repositório no GitHub entre barras
-  // Ex: se o link for github.com/usuario/afinator, o base é '/afinator/'
-  base: '/afinator/', 
+  // Ex: se o link for github.com/usuario/Afinator, o base é '/Afinator/'
+  base: '/Afinator/', 
 })
